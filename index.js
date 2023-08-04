ac209c45fecdf5167ea5715841942b8a1ca6ef33
MARY NJOKI <marynjoki738@gmail.com>
2023-08-04 14:43:17 +0300

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   
     async function fetchData(query) {
       try {
-        const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+        const res = await fetch(`${url}${query}&apiKey=55c6498d47dc42e58594ea740046d649`);
         const data = await res.json();
         return data;
       } catch (error) {
